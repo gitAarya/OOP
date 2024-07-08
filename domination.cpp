54d176901d416c6b79aa0d50daf4ccc356fc154c
gitAarya <gitaarya@gmail.com>
2024-07-08 14:10:06 +0545

    upadted

--- domination.cpp
+++ domination.cpp
@@ -1,8 +1,11 @@
-#include<iostream>
-enum year{jan ,feb,march,april,may,june,july,aug,sept,oct,nov dec};
-void main(){
+#include <iostream>
+
+enum Year { jan, feb, march, april, may, june, july, aug, sept, oct, nov, dec };
+
+int main() {
     int i;
-    for(i=jan;i,=dec;i++){
-        std ::cout<< i<<"";
+    for (i = jan; i <= dec; i++) {
+        std::cout << i << " ";
     }
+    return 0;
 }
