9d1d3ac979bbcdd83455b42c408636997d1a642a
gitAarya <gitaarya@gmail.com>
2024-07-25 12:37:22 +0545

    floor celling program added

---
+++ celling.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float val;
+    float fval, cval;
+    
+    printf("Enter the float value: ");
+    scanf("%f", &val);
+    
+    fval = floor(val);
+    cval = ceil(val);
+    #include <stdio.h>
+#include <math.h>
+
+int main() {
+    float val;
+    float fval, cval;
+    
+    printf("Enter the float value: ");
+    scanf("%f", &val);
+    
+    fval = floor(val);
+    cval = ceil(val);
+    
+    printf("Floor value = %f\nCeiling value = %f\n", fval, cval);
+    
+    return 0;
+}
+
+    printf("Floor value = %f\nCeiling value = %f\n", fval, cval);
+    
+    return 0;
+}
